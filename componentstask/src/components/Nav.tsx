2c20a96c856fbe4f845a30276fe5f5a50d2fbae3
Kiryll Khomchanka <dickdurex265@gmail.com>
2023-03-15 19:34:40 +0300

    feat: add products array

---
+++ componentstask/src/components/Nav.tsx
@@ -0,0 +1,8 @@
+export function Nav() {
+  return (
+<nav>
+  <a href="#card">Card</a>
+  <a href="#about">About</a>
+</nav>
+  )
+}
--- componentstask/src/components/header.tsx
+++ componentstask/src/components/header.tsx
@@ -1,9 +1,11 @@
+import { Nav } from "./Nav";
 import { SearchBar } from "./SearchBar";
 
 export function Header() {
   return (
     <header>
       <SearchBar></SearchBar>
+      <Nav></Nav>
     </header>
   )
 }
